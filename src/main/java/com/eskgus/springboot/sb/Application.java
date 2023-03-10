57cfa925be4f5c7548918ff1199ee2691743729c
eskgus <eskgus012@naver.com>
2023-03-10 21:23:09 +0900

    Jar (자동) 배포

--- src/main/java/com/eskgus/springboot/sb/Application.java
+++ src/main/java/com/eskgus/springboot/sb/Application.java
@@ -2,10 +2,17 @@ package com.eskgus.springboot.sb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication  // 스프링 부트의 자동 설정, 스프링 bean 읽기/생성 모두 자동으로 설정
 public class Application {  // 프로젝트의 메인 클래스
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args); // 내장 WAS 실행
+
+        // 새 파일이 commit 되면 이전 버전 중단시키고 업데이트 된 jar 실행하기 위해 application.properties에서 설정한
+        // pid commend 값을 pid commend로 사용하려고 수정
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
+
     }
 }
